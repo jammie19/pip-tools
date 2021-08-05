3aba1f89acdc0927f928674f78162d35d3b18979
Andy Kluger <andykluger@gmail.com>
2021-08-05 15:25:29 -0400

    add optional from_dir param to parse_requirements and ensure _was_relative accuracy
    If passed, that folder will be used instead of the cwd for interpreting relative paths.
    
    Special care is taken to ensure _was_relative in the yielded ireqs if originally relative.
    
    When calling format_requirement, _was_relative's presence now determines what's passed
    as from_dir: either CWD or None (None leads to an absolute path output)

--- piptools/_compat/pip_compat.py
+++ piptools/_compat/pip_compat.py
@@ -1,4 +1,6 @@
 import optparse
+import platform
+import re
 from typing import Iterator, Optional
 
 import pip
@@ -9,7 +11,11 @@ from pip._internal.req import parse_requirements as _parse_requirements
 from pip._internal.req.constructors import install_req_from_parsed_requirement
 from pip._vendor.packaging.version import parse as parse_version
 
+from ..utils import abs_ireq, working_dir
+
 PIP_VERSION = tuple(map(int, parse_version(pip.__version__).base_version.split(".")))
+
+file_url_schemes_re = re.compile(r"^((git|hg|svn|bzr)\+)?file:")
 
 
 def parse_requirements(
@@ -19,8 +25,33 @@ def parse_requirements(
     options: Optional[optparse.Values] = None,
     constraint: bool = False,
     isolated: bool = False,
+    from_dir: Optional[str] = None,
 ) -> Iterator[InstallRequirement]:
     for parsed_req in _parse_requirements(
         filename, session, finder=finder, options=options, constraint=constraint
     ):
-        yield install_req_from_parsed_requirement(parsed_req, isolated=isolated)
+        # This context manager helps pip locate relative paths specified
+        # with non-URI (non file:) syntax, e.g. '-e ..'
+        with working_dir(from_dir):
+            ireq = install_req_from_parsed_requirement(parsed_req, isolated=isolated)
+
+        # But the resulting ireq is absolute (ahead of schedule),
+        # so abs_ireq will not apply the _was_relative attribute,
+        # which is needed for the writer to use the relpath.
+        a_ireq = abs_ireq(ireq, from_dir)
+
+        # To account for that, we guess if the path was initially relative and
+        # set _was_relative ourselves:
+        bare_path = file_url_schemes_re.sub("", parsed_req.requirement)
+        is_win = platform.system() == "Windows"
+        if is_win:
+            bare_path = bare_path.lstrip("/")
+        if (
+            a_ireq.link is not None
+            and a_ireq.link.scheme.endswith("file")
+            and not bare_path.startswith("/")
+        ):
+            if not (is_win and re.match(r"[a-zA-Z]:", bare_path)):
+                a_ireq._was_relative = True
+
+        yield a_ireq
